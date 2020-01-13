a92e4a06b345b41b6fceb8af3c57cf32a5d0837a
Roby Baiaturridwan Suryana <rb.aturridwan@gmail.com>
2020-01-13 18:01:09 +0700

    sebelum ditambah StateOrientation

--- app/src/main/java/com/geeksfarm/training/counterapp/MainActivity.java
+++ app/src/main/java/com/geeksfarm/training/counterapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.geeksfarm.training.counterapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
@@ -12,15 +11,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private final String STATE_COUNT = "state_count"; // konstanta untuk state count
-    private final String STATE_LAST_ORIENTATION = "state_last_orientation";
 
     //pembuatan object component
     TextView tvCount;
     Button btnCount, btnReset;
-    int count = 998; //tipe data primitif
+    int count = 0; //tipe data primitif
     //Integer count = 0; //tipe data reference
-
-    int last_orientation = 1; // 1 for portrait
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,34 +28,9 @@ public class MainActivity extends AppCompatActivity {
         btnCount = findViewById(R.id.btn_count);
         btnReset = findViewById(R.id.btn_reset);
 
-        if(getResources().getConfiguration().orientation == 1){ // jika orientasi saat ini adalah portrait
-            last_orientation = 2; // last nya adalah landscape
-        }
 
         if(savedInstanceState != null){ //cek apakah ada state yang tersimpan ?
             count = savedInstanceState.getInt(STATE_COUNT);
-            last_orientation = savedInstanceState.getInt(STATE_LAST_ORIENTATION);
-            if(count >= 1000){
-                //set font size
-                if(last_orientation == Configuration.ORIENTATION_LANDSCAPE){
-                    //if last is landscape and now is portrait
-                    tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.smaller_text_tv_count));
-                }
-                else{
-                    //if last is portrait and now is lands
-                    tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.default_text_tv_count_land));
-                }
-            }
-            else{
-                if(last_orientation == Configuration.ORIENTATION_LANDSCAPE){
-                    //if last is landscape and now is portrait
-                    tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.default_text_tv_count));
-                }
-                else{
-                    //if last is portrait and now is lands
-                    tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.default_text_tv_count_land));
-                }
-            }
             tvCount.setText(String.valueOf(count));
         }
 
@@ -70,14 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 //do something here
                 count++;
                 if(count == 1000) {
-                    if(last_orientation == Configuration.ORIENTATION_LANDSCAPE){
-                        //if last is landscape and now is portrait
                         tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.smaller_text_tv_count));
-                    }
-                    else{
-                        //if last is portrait and now is lands
-                        tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.default_text_tv_count_land));
-                    }
 
                 }
                 tvCount.setText(String.valueOf(count));
@@ -90,15 +54,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 count = 0;
-
-                if(last_orientation == Configuration.ORIENTATION_LANDSCAPE){
-                    //if last is landscape and now is portrait
-                    tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.default_text_tv_count));
-                }
-                else{
-                    //if last is portrait and now is lands
-                    tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.default_text_tv_count_land));
-                }
+                tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.default_text_tv_count));
 
                 tvCount.setText(String.valueOf(count));
             }
@@ -113,6 +69,5 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         //save state
         outState.putInt(STATE_COUNT,count);
-        outState.putInt(STATE_LAST_ORIENTATION,getResources().getConfiguration().orientation);
     }
 }
